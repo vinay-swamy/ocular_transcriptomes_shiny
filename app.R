298031933bf9a764dc80cbc19ec9d2a5fe850e98
Swamy <swamyvinny@gmail.com>
2020-01-08 14:48:08 -0500

    have a working version of app that feeds from pipeline. currenlty showing transcript bodies, piu and num samp det. no reactivity yet

---
+++ app.R
@@ -0,0 +1,76 @@
+library(shiny)
+
+#load('~/NIH/ocular_transcriptomes_shiny/data/new_data_dev.Rdata')
+load('~/NIH/ocular_transcriptomes_shiny/data/numdet.Rdata')
+source('~/NIH/ocular_transcriptomes_shiny/src/draw_transcripts.R')
+source('~/NIH/ocular_transcriptomes_shiny/src/plotting_funcs.R')
+subtissues <- colnames(dev_piu)[-(1:2)]
+
+ui <- fluidPage(
+    titlePanel("Test App"),
+    fluidRow(
+        column(4,#----
+               selectInput(inputId = 'u_gene',
+                           label = 'Enter Gene Name',
+                           choices = dev_gene_names,
+                           #selected = 'CENPS',
+                           selected = "TMEM56",
+                           multiple = F)
+               
+        ),
+        column(4,#----
+               selectInput(inputId = 'u_tissues',
+                           label = 'Enter Gene Name',
+                           choices = dev_subtissues,
+                           selected = 'Retina_Adult.Tissue',
+                           multiple = T)
+        )
+    ),
+    
+    plotOutput('piu_plot', height = 500),
+    uiOutput('tx_diag'),
+    plotOutput('num_samp_det')
+    
+    
+)#----
+
+
+server <- function(input, output) {
+    #load('~/personal/shiny_practice/data/gtf.Rdata')
+    
+    output$tx_diag <- renderUI({
+        s_gene <- input$u_gene
+        s_tissues <- input$u_tissues
+        keep_tx <- dev_tissue_det %>% select(s_tissues) %>% {rowSums(.) >0} %>% {dev_tissue_det[.,]} %>% pull(transcript_id)
+        
+        
+        if(length(keep_tx) == 0){
+            output$warning <- renderText('Gene not expressed in selected tissues')
+            textOutput('warning')
+        }else {
+            height <- get_num_uniq_tx(gene = s_gene, gtf = dev_gtf,keep_tx = keep_tx)
+            
+            title <- 'All Transcripts'
+            
+            output$piu_plot <- renderPlot({
+                make_piu_bargraph(gene = s_gene, tissues = s_tissues, keep_tx = keep_tx,piu = dev_piu)
+            }, height = 500)
+            
+            output$transcript_diag <- renderPlot({
+                draw_all_transcripts_static(gene = s_gene, tissues=s_tissues, gtf=dev_gtf,keep_tx = keep_tx) 
+                
+            }, height = 25*(height+1) )
+            
+            output$num_samp_det <- renderPlot({
+                make_num_det_bargraph(gene = s_gene, tissues = s_tissues, num_det = dev_frac_det, keep_tx = keep_tx)
+            })
+
+
+            plotOutput('transcript_diag')
+        }
+    })
+    
+    
+}
+
+shinyApp(ui, server)
