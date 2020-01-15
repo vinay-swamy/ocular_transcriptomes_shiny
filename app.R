5f3adcbc7000ba0e00f3f697935c634488012355
Swamy <swamyvinny@gmail.com>
2020-01-15 12:09:16 -0500

    things are working mostly the way they should, so going to start adding the sql backend parts

--- app.R
+++ app.R
@@ -1,34 +1,36 @@
 library(shiny)
 
 #load('~/NIH/ocular_transcriptomes_shiny/data/new_data_dev.Rdata')
-load('~/NIH/ocular_transcriptomes_shiny/data/numdet.Rdata')
+load('~/NIH/ocular_transcriptomes_shiny/data/V2_shinydata.Rdata')
 source('~/NIH/ocular_transcriptomes_shiny/src/draw_transcripts.R')
 source('~/NIH/ocular_transcriptomes_shiny/src/plotting_funcs.R')
-subtissues <- colnames(dev_piu)[-(1:2)]
+#subtissues <- colnames(dev_piu)[-(1:2)]
 
 ui <- fluidPage(
-    titlePanel("Test App"),
+    titlePanel("De Novo Transcriptomes"),
     fluidRow(
         column(4,#----
                selectInput(inputId = 'u_gene',
                            label = 'Enter Gene Name',
                            choices = dev_gene_names,
                            #selected = 'CENPS',
-                           selected = "TMEM56",
+                           selected = "USP1",
                            multiple = F)
                
         ),
         column(4,#----
                selectInput(inputId = 'u_tissues',
-                           label = 'Enter Gene Name',
+                           label = 'Select up to 5 tissues',
                            choices = dev_subtissues,
-                           selected = 'Retina_Adult.Tissue',
+                           selected = c('Retina_Adult.Tissue', 'RPE_Adult.Tissue'),
                            multiple = T)
         )
     ),
-    
+    h2('Percentage of total gene expression attributed to its transcripts expressed in selected tissues'),
     plotOutput('piu_plot', height = 500),
+    h3('Exon Diagram of Transcripts for selected gene'),
     uiOutput('tx_diag'),
+    h4('Fraction of samples each transcript was constructed in'),
     plotOutput('num_samp_det')
     
     
@@ -64,8 +66,6 @@ server <- function(input, output) {
             output$num_samp_det <- renderPlot({
                 make_num_det_bargraph(gene = s_gene, tissues = s_tissues, num_det = dev_frac_det, keep_tx = keep_tx)
             })
-
-
             plotOutput('transcript_diag')
         }
     })
@@ -74,3 +74,6 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
+
+#example gene USP1
