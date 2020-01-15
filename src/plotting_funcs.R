154d226f8ca26f2241703b4af3d05bc447d0877c
Swamy <swamyvinny@gmail.com>
2020-01-15 12:10:03 -0500

    moved titles out of here into app.R file

--- src/plotting_funcs.R
+++ src/plotting_funcs.R
@@ -14,7 +14,8 @@ make_piu_bargraph <- function(gene, piu, tissues,keep_tx){
           gather(key='subtissue', value = 'piu', -transcript_id, -gene_name)
       plot <- ggplot(data = exp_piu) +
           geom_col(aes(x=transcript_id, y=piu, fill=subtissue), position = 'dodge')+
-          ggtitle('fraction of total gene expression attributed to each transcript in selected tissues')+
+          #ggtitle('fraction of total gene expression attributed to each transcript in selected tissues')+
+          ylim(c(0,1)) +
           theme_minimal() +
           theme(axis.text.x=element_text(angle=45, hjust = 1))
       return(plot)
@@ -28,7 +29,8 @@ make_num_det_bargraph <- function(gene, num_det, tissues, keep_tx){
         gather(key='subtissue', value = 'Fraction of Samples', -transcript_id, -gene_name)
     plot <- ggplot(data = exp_numdet) +
         geom_col(aes(x=transcript_id, y=`Fraction of Samples`, fill=subtissue), position = 'dodge')+
-        ggtitle('fraction of samples transcript was contructed in for selected tissues')+
+        #ggtitle('fraction of samples transcript was contructed in for selected tissues')+
+        ylim(c(0,1)) +
         theme_minimal() +
         theme(axis.text.x=element_text(angle=45, hjust = 1))
     return(plot)
