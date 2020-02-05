fa1d9678fc0a50f24077fb1687d8510b49e779ad
Swamy <swamyvinny@gmail.com>
2020-02-05 15:21:37 -0500

    Download data works now

--- app.R
+++ app.R
@@ -2,42 +2,74 @@ library(shiny)
 suppressMessages(library(tidyverse))
 library(DBI)
 
-load('~/NIH/ocular_transcriptomes_shiny/data/shiny_data.Rdata')
-source('~/NIH/ocular_transcriptomes_shiny/src/draw_transcripts.R')
-source('~/NIH/ocular_transcriptomes_shiny/src/plotting_funcs.R')
+load('app_data/shiny_data.Rdata')
+source('src/draw_transcripts.R')
+source('src/plotting_funcs.R')
 
-db_name <- '~/NIH/ocular_transcriptomes_shiny/data/DNTX_db.sql'
+db_name <- 'app_data/DNTX_db.sql'
 con <- dbConnect(RSQLite::SQLite(),db_name)
 
 ui <- fluidPage(
     titlePanel("De Novo Transcriptomes"),
-    fluidRow(
-        column(4,#----
-               selectInput(inputId = 'u_gene',
-                           label = 'Enter Gene Name',
-                           choices = all_gene_names,
-                           selected = "USP1",
-                           multiple = F)
-               
-        ),
-        column(4,#----
-               selectizeInput(inputId = 'u_tissues',
-                           label = 'Select up to 5 tissues',
-                           options = list(maxItems = 5),
-                           choices = all_tissues,
-                           selected = c('Retina_Adult.Tissue', 'RPE_Adult.Tissue'),
-                           multiple = T)
-        ),
-        column(2, actionButton('draw', label = 'click to redraw plot' )),
-        column(2, downloadButton(outputId = 'download', label = 'download combined transcriptome for selected tissues'))
-    ),
-    h2('Percentage of total gene expression attributed to each transcript expressed in selected tissues'),
-    plotOutput('piu_plot', height = 500),
-    h3('Exon Diagram of Transcripts for selected gene'),
-    uiOutput('tx_diag'),
-    h4('Fraction of samples each transcript was constructed in'),
-    plotOutput('num_samp_det')
-    
+    tabsetPanel(
+        tabPanel('plots', #----
+            fluidRow(
+                column(4,
+                       selectInput(inputId = 'u_gene',
+                                   label = 'Enter Gene Name',
+                                   choices = all_gene_names,
+                                   selected = "USP1",
+                                   multiple = F)
+                       
+                ),
+                column(4,
+                       selectizeInput(inputId = 'u_tissues',
+                                   label = 'Select up to 5 tissues',
+                                   options = list(maxItems = 5),
+                                   choices = all_tissues,
+                                   selected = c('Retina_Adult.Tissue', 'RPE_Adult.Tissue'),
+                                   multiple = T)
+                ),
+                column(2, actionButton('draw', label = 'click to redraw plot' ))
+                
+            ),
+            h2('Percentage of total gene expression attributed to each transcript expressed in selected tissues'),
+            plotOutput('piu_plot', height = 500),
+            h3('Exon Diagram of Transcripts for selected gene'),
+            uiOutput('tx_diag'),
+            h4('Fraction of samples each transcript was constructed in'),
+            plotOutput('num_samp_det')
+        ),#----
+        tabPanel('download', 
+                    h2('Download De Novo Transcriptomes'),
+                    column(3, radioButtons(inputId = 'dl_base_choice',
+                                           label = 'select an option', 
+                                           c('pan-body(54 subtissues)' = 'panbody', 
+                                             'pan-eye(6 subtissues)'  = 'paneye',
+                                             'by subtissue(eye only)' = 'subtissue')
+                                           ),
+                           downloadButton(outputId = 'dl_download',
+                                          label = 'download')
+                            ),
+                    column(3, conditionalPanel("input.dl_base_choice == 'subtissue'",
+                                               selectInput(inputId = 'dl_tis_choice', 
+                                                           label = 'select a tissue',
+                                                           choices = c('Retina', 'RPE', 'Cornea'),
+                                                           selected = NULL
+                                                           ),
+                                               checkboxGroupInput(inputId = 'dl_dev_choice', 
+                                                            label = NULL, 
+                                                            choices = c('Adult' = 'Adult', 
+                                                                        'Fetal' = 'Fetal'),
+                                                            selected = 'Adult')
+                                                           
+                                               )
+                           
+                           )
+
+                 )
+        
+    )
     
 )#----
 
@@ -65,12 +97,6 @@ server <- function(input, output, session) {
 
             height <- get_num_uniq_tx(gene = s_gene, gtf = s_gtf,keep_tx = s_keep_tx)
             
-            data <- mtcars
-            output$download <- downloadHandler(
-                filename = function() {paste0(paste(s_tissues, collapse = '-'), '.Rdata') },
-                content = function(file){save(data, file =file)}
-                )
-            
             output$piu_plot <- renderPlot({
                 make_piu_bargraph(gene = s_gene, tissues = s_tissues, keep_tx = s_keep_tx,piu = s_piu)
             }, height = 500)
@@ -92,6 +118,26 @@ server <- function(input, output, session) {
     
         }
     })
+    output$dl_download <- downloadHandler(
+        filename = function(){
+            if(input$dl_base_choice == 'subtissue'){
+                paste0(paste0(input$dl_tis_choice,'_', input$dl_dev_choice, '.Tissue', collapse = '-'), '.tar.gz', collapse = '-')
+            } else {
+                paste0(input$dl_base_choice, '.tar.gz')
+            }
+        },
+        content = function(file){
+            
+            if(input$dl_base_choice == 'subtissue'){
+                fn=paste0('dl_data/', paste0(input$dl_tis_choice,'_', input$dl_dev_choice, '.Tissue', collapse = '-'), '.tar.gz', collapse = '-')
+            } else {
+                fn=paste0('dl_data/', input$dl_base_choice, '.tar.gz')
+            }
+            stopifnot(file.exists(fn))
+            file.copy(from = fn, to = file)
+        }
+        
+    )
 }
 
 onStop(function() dbDisconnect(con))
